40df9e6667209c188fba5d2e61b3daef3343a21c
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-03-14 16:42:34 +0900

    📦 Chore : redux 초기설정

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { compose, applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "../reducers/index";
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+
+export default store;
