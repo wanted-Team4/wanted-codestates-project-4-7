6daa4620bbf3d4af5d45a364d25d5ab0d6d3cc27
Kth <vcro12123@naver.com>
2022-03-15 03:46:43 +0900

    :sparkles: feat :  파일 업로드 기능 추가

--- src/Page/CreatedForm.js
+++ src/Page/CreatedForm.js
@@ -1,45 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
+import axios from 'axios';
 const CreatedForm = () => {
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState('');
+
+  const saveFile = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const uploadFile = async (e) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', fileName);
+    try {
+      const res = await axios.post('http://localhost:3000/upload', formData);
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+  console.log(fileName);
   return (
     <Container>
       <Title>데이터블 폼 예시</Title>
       <Form>
         <Input>
-          <label for="name">이름</label>
-          <input type="text" placeholder="" id="name" />
+          <label htmlFor='name'>이름</label>
+          <input type='text' placeholder='' id='name' />
         </Input>
         <Input>
-          <label for="phone">휴대폰번호</label>
-          <input type="number" placeholder="" id="phone" />
+          <label htmlFor='phone'>휴대폰번호</label>
+          <input type='number' placeholder='' id='phone' />
         </Input>
         <Input>
-          <label for="address">배송지</label>
-          <input value="{/*배송지*/}" id="address" />
+          <label htmlFor='address'>배송지</label>
+          <input value='{/*배송지*/}' id='address' />
         </Input>
         <Select>
-          <label for="option">옵션</label>
-          <select id="option">
+          <label htmlFor='option'>옵션</label>
+          <select id='option'>
             {/*받아온데이터맵돌리기*/}
-            <option value="{/*선택1*/}">선택1</option>
-            <option value="{/*선택1*/}">선택1</option>
-            <option value="{/*선택1*/}">선택1</option>
-            <option value="{/*선택1*/}">선택1</option>
+            <option value='{/*선택1*/}'>선택1</option>
+            <option value='{/*선택1*/}'>선택1</option>
+            <option value='{/*선택1*/}'>선택1</option>
+            <option value='{/*선택1*/}'>선택1</option>
           </select>
         </Select>
-        <File className="file">
-          <label for="file">
-            <i class="fa-solid fa-plus"></i>
+        <File className='file'>
+          <label htmlFor='file'>
+            <i className='fa-solid fa-plus'></i>
             <span>눌러서 파일 등록</span>
+            <div>
+              <b>업로드파일</b>
+            </div>
+            <span>{fileName}</span>
           </label>
-          <input type="file" id="file" />
+          <input type='file' id='file' onChange={saveFile} />
         </File>
-        <Checkbox className="checkbox">
-          <input type="checkbox" id="checkbox" />
-          <label for="checkbox">개인정보 수집 약관 동의(필수)</label>
+        <Checkbox className='checkbox'>
+          <input type='checkbox' id='checkbox' />
+          <label htmlFor='checkbox'>개인정보 수집 약관 동의(필수)</label>
         </Checkbox>
-        <Submit type="submit">제출하기</Submit>
+        <Submit type='submit' onClick={uploadFile}>
+          제출하기
+        </Submit>
       </Form>
     </Container>
   );
