036584d3476924c7b34be40f574b8b2ad855cd07
sunysty <seokjiseon@gmail.com>
2022-03-15 12:27:54 +0900

    ✨  feat : 리덕스 state값 가져와서 뿌려주기

--- src/Page/CreatedForm.js
+++ src/Page/CreatedForm.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+
 const CreatedForm = () => {
   const [file, setFile] = useState();
   const [fileName, setFileName] = useState('');
-
+  const dispatch = useDispatch();
   const saveFile = (e) => {
     setFile(e.target.files[0]);
     setFileName(e.target.files[0].name);
   };
-
   const uploadFile = async (e) => {
     const formData = new FormData();
     formData.append('file', file);
@@ -21,49 +23,67 @@ const CreatedForm = () => {
       console.log(ex);
     }
   };
-  console.log(fileName);
+  const HandleSubmit = (e) => {
+    uploadFile(e);
+    dispatch();
+  };
+
+  const location = useLocation();
+  const path = location.pathname.split('/');
+  const createdFormId = path[path.length - 1];
+
+  const { forms } = useSelector((state) => state.surveyReducer);
+  const [formData] = forms.filter((el) => el.formId == createdFormId);
+  const { title, fields, formId } = formData;
+  console.log(formData);
+  const [name] = fields.filter((el) => el.id === 'name');
+  const [phone] = fields.filter((el) => el.id === 'phone');
+  const [address] = fields.filter((el) => el.id === 'address');
+  const [select] = fields.filter((el) => el.id === 'input_0');
+  const option = select.options;
+  const [fileData] = fields.filter((el) => el.id === 'input_1');
+
   return (
     <Container>
-      <Title>데이터블 폼 예시</Title>
+      <Title>{title}</Title>
       <Form>
         <Input>
-          <label htmlFor='name'>이름</label>
-          <input type='text' placeholder='' id='name' />
+          <label htmlFor="name">{name.label}</label>
+          <input type={name.type} placeholder={name.placeholder} id="name" />
         </Input>
         <Input>
-          <label htmlFor='phone'>휴대폰번호</label>
-          <input type='number' placeholder='' id='phone' />
+          <label htmlFor="phone">{phone.label}</label>
+          <input type={phone.type} placeholder={phone.placeholder} id="phone" />
         </Input>
         <Input>
-          <label htmlFor='address'>배송지</label>
-          <input value='{/*배송지*/}' id='address' />
+          <label htmlFor="address">{address.label}</label>
+          <input
+            type={address.type}
+            placeholder={address.placeholder}
+            id="address"
+          />
         </Input>
         <Select>
-          <label htmlFor='option'>옵션</label>
-          <select id='option'>
-            {/*받아온데이터맵돌리기*/}
-            <option value='{/*선택1*/}'>선택1</option>
-            <option value='{/*선택1*/}'>선택1</option>
-            <option value='{/*선택1*/}'>선택1</option>
-            <option value='{/*선택1*/}'>선택1</option>
+          <label htmlFor="option">옵션</label>
+          <select>
+            {option.map((el, i) => (
+              <option key={i}>{select.options[i]} </option>
+            ))}
           </select>
         </Select>
-        <File className='file'>
-          <label htmlFor='file'>
-            <i className='fa-solid fa-plus'></i>
+        <File className="file">
+          <label htmlFor="file">
+            <i className="fa-solid fa-plus"></i>
             <span>눌러서 파일 등록</span>
-            <div>
-              <b>업로드파일</b>
-            </div>
             <span>{fileName}</span>
           </label>
-          <input type='file' id='file' onChange={saveFile} />
+          <input type="file" id="file" onChange={saveFile} />
         </File>
-        <Checkbox className='checkbox'>
-          <input type='checkbox' id='checkbox' />
-          <label htmlFor='checkbox'>개인정보 수집 약관 동의(필수)</label>
+        <Checkbox className="checkbox">
+          <input type="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">개인정보 수집 약관 동의(필수)</label>
         </Checkbox>
-        <Submit type='submit' onClick={uploadFile}>
+        <Submit type="submit" onClick={HandleSubmit}>
           제출하기
         </Submit>
       </Form>
@@ -94,11 +114,12 @@ const Form = styled.form`
   select {
     width: 100%;
     margin-bottom: 1rem;
-    padding: 0.6rem 0.8rem;
+    padding: 0.8rem;
     box-sizing: border-box;
     font-size: 1rem;
     border-radius: 0.5rem;
-    border: 1px solid #999;
+    background: #eee;
+    border: none;
   }
 `;
 
@@ -110,8 +131,8 @@ const File = styled.div`
     position: absolute;
     width: 100%;
     height: 100%;
-    border: 1px solid #999;
-    background: #fff;
+    border: none;
+    background: #eee;
     border-radius: 0.5rem;
     display: flex;
     flex-direction: column;
@@ -134,7 +155,7 @@ const Checkbox = styled.div`
 const Submit = styled.button`
   width: 100%;
   font-size: 1rem;
-  background: #000;
+  background: #ea6157;
   color: #fff;
   padding: 0.8rem;
   border-radius: 0.5rem;
